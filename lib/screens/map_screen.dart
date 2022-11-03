8b6d44fe3103ab97466889333601a9caf4b94c8f
Sam Cartledge <scartledge@esri.com>
2022-11-03 13:36:48 -0700

    Update map_screen.dart

--- lib/screens/map_screen.dart
+++ lib/screens/map_screen.dart
@@ -43,60 +43,33 @@ class _MapScreenState extends State<MapScreen> {
             ),
         ],
       ),
-      body: InteractiveViewer(
-        boundaryMargin: const EdgeInsets.all(20.0),
-        minScale: 0.1,
-        maxScale: 1.6,
-        child: GoogleMap(
-          zoomGesturesEnabled: true,
-          tiltGesturesEnabled: false,
-          onCameraMove: (CameraPosition cameraPosition) {
-            print(cameraPosition.zoom);
-          },
-          initialCameraPosition: CameraPosition(
-            target: LatLng(
-              widget.initialLocation.latitude,
-              widget.initialLocation.longitude,
-            ),
-            zoom: 16,
+      body: GoogleMap(
+        zoomGesturesEnabled: true,
+        tiltGesturesEnabled: false,
+        onCameraMove: (CameraPosition cameraPosition) {
+          print(cameraPosition.zoom);
+        },
+        initialCameraPosition: CameraPosition(
+          target: LatLng(
+            widget.initialLocation.latitude,
+            widget.initialLocation.longitude,
           ),
-          onTap: widget.isSelecting ? _selectLocation : null,
-          markers: (_pickedLocation == null && widget.isSelecting)
-              ? {}
-              : {
-                  Marker(
-                    markerId: MarkerId('m1'),
-                    position: _pickedLocation ??
-                        LatLng(
-                          widget.initialLocation.latitude,
-                          widget.initialLocation.longitude,
-                        ),
-                  ),
-                },
+          zoom: 16,
         ),
+        onTap: widget.isSelecting ? _selectLocation : null,
+        markers: (_pickedLocation == null && widget.isSelecting)
+            ? {}
+            : {
+                Marker(
+                  markerId: MarkerId('m1'),
+                  position: _pickedLocation ??
+                      LatLng(
+                        widget.initialLocation.latitude,
+                        widget.initialLocation.longitude,
+                      ),
+                ),
+              },
       ),
-      // body: GoogleMap(
-      //   initialCameraPosition: CameraPosition(
-      //     target: LatLng(
-      //       widget.initialLocation.latitude,
-      //       widget.initialLocation.longitude,
-      //     ),
-      //     zoom: 16,
-      //   ),
-      //   onTap: widget.isSelecting ? _selectLocation : null,
-      //   markers: (_pickedLocation == null && widget.isSelecting)
-      //       ? {}
-      //       : {
-      //           Marker(
-      //             markerId: MarkerId('m1'),
-      //             position: _pickedLocation ??
-      //                 LatLng(
-      //                   widget.initialLocation.latitude,
-      //                   widget.initialLocation.longitude,
-      //                 ),
-      //           ),
-      //         },
-      // ),
     );
   }
 }
