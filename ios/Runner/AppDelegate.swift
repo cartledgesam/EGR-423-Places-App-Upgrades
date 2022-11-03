ebe0262244f2ffc5ebd25fa718f1fecbc62146dd
Sam Cartledge <scartledge@esri.com>
2022-11-03 13:09:19 -0700

    fixes

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -7,6 +7,7 @@ import Flutter
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+    GMSServices.provideAPIKey("AIzaSyCzl2lhm9VBfrHo4NohSt-B3-hif6PGWUw")
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
